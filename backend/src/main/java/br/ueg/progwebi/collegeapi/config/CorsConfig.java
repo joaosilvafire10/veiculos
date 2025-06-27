fca3c654d7e63bdc69fa63618bdc4ad4d40e90a1
Joao Victor <99901008+joaosilvafire10@users.noreply.github.com>
2025-06-27 10:39:00 -0300

    integração Back e Frontend
    Vou realizado o ajuste para que possa realizar a edição. 
    
    E também foi ajustado o application.properties, para ao recarregar o back ele não limpasse a tabela veículos.

--- backend/src/main/java/br/ueg/progwebi/collegeapi/config/CorsConfig.java
+++ backend/src/main/java/br/ueg/progwebi/collegeapi/config/CorsConfig.java
@@ -1,18 +1,15 @@
 package br.ueg.progwebi.collegeapi.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // todas as rotas
-                .allowedOrigins("http://localhost:63342") // origem do seu frontend
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:63342") // Ou a porta do seu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
